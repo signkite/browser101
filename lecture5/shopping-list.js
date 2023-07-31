b2155f201e7418a984d0009d977028e4c8d23142
signkite <hyshin1997@naver.com>
2023-07-31 14:11:15 +0900

    shoping list 업그레이드

--- lecture5/shopping-list.js
+++ lecture5/shopping-list.js
@@ -2,12 +2,15 @@ const ulElement = document.querySelector('ul');
 const inputElement = document.querySelector('input');
 const buttomElement = document.querySelector('button');
 
-buttomElement.addEventListener('click', () => {
+function addItem() {
   const listItem = document.createElement('li');
   const itemName = document.createElement('div');
   const delButton = document.createElement('button');
 
   itemName.classList.add('list-name');
+
+  if (!inputElement.value)
+    return;
 
   itemName.innerHTML = inputElement.value;
   delButton.innerHTML = '삭제'
@@ -21,11 +24,18 @@ buttomElement.addEventListener('click', () => {
   ulElement.appendChild(listItem);
 
   inputElement.value = '';
+  inputElement.focus();
+}
+
+inputElement.addEventListener('keypress', (event) => {
+  if(event.key === 'Enter')
+    addItem();
+})
+
+buttomElement.addEventListener('click', () => {
+  addItem();
 })
 
 // todo
-// 자동으로 input focus 되도록
-// 그림자 추가
-// input에 엔터키 누르면 추가되도록
 // input에 아무것도 없으면 추가되지 않도록
 // 사용자가 많이 입력하면 자동으로 아래로 스크롤
